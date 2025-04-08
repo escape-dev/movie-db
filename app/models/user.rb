869dbe5b852a1ea792e1ee464585f6de07230b14
Muhammad Hafiz <hafiz170301@gmail.com>
2025-04-08 10:35:55 +0700

    feat: adding secure password and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, :email, :password, presence: true
+  validates :name, length: { maximum: 35 }
+  validates :name, format: /\A[^0-9`!@#\$%\^&*+_=]+\z/
+  validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
